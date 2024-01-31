b837ceda5435dc8a72e2d9640b67480a9fbcb360
Tom <zlac316@live.rhul.ac.uk>
2024-01-31 15:45:13 +0000

    Changing folder name to lower case

---
+++ project/src/main/java/cs2815/project/service/implementations/AssetAttributeValueImpl.java
@@ -0,0 +1,25 @@
+package cs2815.project.service.implementations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cs2815.project.model.AssetAttributeValue;
+import cs2815.project.repo.AssetAttributeValueRepo;
+import cs2815.project.service.AssetAttributeValueService;
+
+@Service
+public class AssetAttributeValueImpl implements AssetAttributeValueService {
+
+    @Autowired
+    private AssetAttributeValueRepo repo;
+
+    @Override
+    public void createAssetAttributeValue(AssetAttributeValue assetAttributeValue) {
+        if (assetAttributeValue == null) {
+            System.out.println("Error: AttributeValue is null.");
+            return;
+        }
+        repo.save(assetAttributeValue);
+    }
+
+}
