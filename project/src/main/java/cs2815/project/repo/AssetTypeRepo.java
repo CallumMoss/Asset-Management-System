4730e102cf6bf841023074965c8b9286d6333d46
Tom <zlac316@live.rhul.ac.uk>
2024-02-05 12:14:31 +0000

    changes to folder name

--- project/src/main/java/cs2815/project/repo/AssetTypeRepo.java
+++ project/src/main/java/cs2815/project/repo/AssetTypeRepo.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface AssetTypeRepo extends JpaRepository<AssetType, String> {
+public interface AssetTypeRepo extends JpaRepository<AssetType, Integer> {
 
 }
