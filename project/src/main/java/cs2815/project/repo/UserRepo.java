30c30affec51cf052960b4a6a3425c0a6252cb78
Tom <zlac316@live.rhul.ac.uk>
2024-02-05 16:51:06 +0000

    Updates for searches

--- project/src/main/java/cs2815/project/repo/UserRepo.java
+++ project/src/main/java/cs2815/project/repo/UserRepo.java
@@ -3,14 +3,43 @@ package cs2815.project.repo;
 import cs2815.project.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @RepositoryRestResource
 public interface UserRepo extends JpaRepository<User, Integer> {
 
+    @Query("SELECT u FROM User u")
+    List<User> getAllUsers();
+
     @Query("SELECT u FROM User u WHERE u.user_name = :userName")
     User findByUserName(@Param("userName") String userName);
 
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM User u WHERE u.id = :userId")
+    void deleteUserById(@Param("userId") int userId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User u SET u.user_name = :newUserName, u.user_first_name = :newFirstName, " +
+            "u.user_last_name = :newLastName, u.user_role = :newUserRole " +
+            "WHERE u.id = :userId")
+    void updateUserFieldsById(@Param("userId") int userId,
+            @Param("newUserName") String newUserName,
+            @Param("newFirstName") String newFirstName,
+            @Param("newLastName") String newLastName,
+            @Param("newUserRole") String newUserRole);
+
+    @Query("UPDATE User u SET u.user_password = :newPassword " +
+            "WHERE u.id = :userId")
+    void resetPassword(@Param("userId") int userId,
+            @Param("newPassword") String newPassword);
+
+    @Query("SELECT u.user_name FROM User u")
+    List<String> findAllUserNames();
 }
--- project/src/main/java/cs2815/project/service/UserService.java
+++ project/src/main/java/cs2815/project/service/UserService.java
@@ -1,5 +1,7 @@
 package cs2815.project.service;
 
+import java.util.List;
+
 import cs2815.project.model.User;
 
 public interface UserService {
@@ -9,5 +11,15 @@ public interface UserService {
     public boolean logIn(User user);
 
     public String getUserRole(String username);
+
+    public void editUser(User user);
+
+    public void deleteUser(int userId);
+
+    public List<User> refreshUser();
+
+    public List<String> searchByUsername(String searchword);
+
+    public void resetPassword(int userId, String newPassword);
 
 }
