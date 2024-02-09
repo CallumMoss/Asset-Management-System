241e7545157ef6ead059991e917192c902cd19c1
Shah Dev (2022) ZLAC257 <dev.shah.2022@live.rhul.ac.uk>
2024-02-09 10:50:41 +0000

    Update AssetImpl.java

--- project/src/main/java/cs2815/project/service/Implementations/AssetImpl.java
+++ project/src/main/java/cs2815/project/service/Implementations/AssetImpl.java
@@ -1,11 +1,14 @@
 package cs2815.project.service.Implementations;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import cs2815.project.model.Asset;
 import cs2815.project.repo.AssetRepo;
 import cs2815.project.service.AssetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class AssetImpl implements AssetService {
@@ -13,15 +16,62 @@ public class AssetImpl implements AssetService {
     @Autowired
     private AssetRepo repo;
 
-    @Transactional
-    public void createAsset(Asset asset, Set<String> languageNames) {
-        // Save the asset
-        Asset savedAsset = assetRepo.save(asset);
+    @Autowired
+    private UserServiceImpl userService;
 
-        // Associate languages with the asset
-        for (String languageName : languageNames) {
-            assetRepo.addLanguageToAsset(savedAsset.getAsset_id(), languageName);
+    public static HashMap<String, Integer> languageIDMap = Asset.languageIDMap;
+
+    @Override
+    public void createAsset(Asset asset) {
+        if (asset == null) {
+            System.out.println("Error: Asset is null.");
+            return;
         }
+        if (asset.getLangList() != null) {
+            String[] langList = asset.getLangList().split("/");
+            for (int i = 0; i < langList.length; i++) {
+                if (i >= 5) {
+                    break;
+                }
+                Integer languageID = languageIDMap.get(langList[i]);
+                if (languageID != null) {
+                    asset.addLanguageID(languageID);
+                } else {
+                    System.out.println("Error: Language ID not found for " + langList[i]);
+                }
+            }
+        }
+        repo.save(asset);
     }
 
+    @Override
+    public List<Asset> searchAsset(String searchString) {
+        List<Asset> assetList = repo.getAllAssets();
+        List<Integer> compatibleLangIDs = new ArrayList<>();
+        List<Asset> compatibleList = new ArrayList<>();
+        for (String language : languageIDMap.keySet()) {
+            userService.isSimilar(searchString, language);
+            if (userService.isSimilar(searchString, language)) {
+                compatibleLangIDs.add(languageIDMap.get(language));
+            }
+        }
+        for (Asset asset : assetList) {
+            if (asset.isSearched(compatibleLangIDs)) {
+                compatibleList.add(asset);
+            }
+        }
+        return compatibleList;
+    }
+
+    @Override
+    public List<String> searchLanguage(String searchString) {
+        List<String> compatibleList = new ArrayList<>();
+        for (String language : languageIDMap.keySet()) {
+            userService.isSimilar(searchString, language);
+            if (userService.isSimilar(searchString, language)) {
+                compatibleList.add(language);
+            }
+        }
+        return compatibleList;
+    }
 }
