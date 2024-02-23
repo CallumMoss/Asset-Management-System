baa09f0d38b426d68b595c08c512757a2e935503
zlac214 <zlac214@live.rhul.ac.uk>
2024-02-23 14:33:29 +0000

    Changed colour of user dropdown items by moving code to correct location in Assets.

--- frontend/src/components/Assets.jsx
+++ frontend/src/components/Assets.jsx
@@ -68,33 +68,7 @@ function Navbar({ userRole }) {
             )}
           </div>
         )}
-
-        {/* Render the User dropdown */}
-        <div className="menu-container" ref={menuRef}>
-          {/* Menu trigger */}
-          <div
-            className="menu-trigger"
-            onClick={() => {
-              setOpen(!open);
-            }}>
-            <img src={user} alt="User Icon" />
-          </div>
-
-          {/* Dropdown menu */}
-          <div className={`dropdown-menu ${open ? "active" : "inactive"}`}>
-            <ul>
-              {/* Rendering DropdownItem components with different destinations */}
-              <DropdownItem
-                img={change_password}
-                text={"Change Password"}
-                destination="/change-password"
-              />
-              <DropdownItem img={logout} text={"Logout"} destination="/login" />
-            </ul>
-          </div>
-        </div>
       </nav>
-      <div className="App">{/* Your other components */}</div>
     </header>
   );
 }
@@ -169,6 +143,28 @@ function Assets({ username, userRole }) {
           <DisplayAssets />
         </section>
       </main>
+      <div className="App">{/* Your other components */}
+        {/* Render the User dropdown */}
+        <div className="menu-container" ref={menuRef}>
+            {/* Menu trigger */}
+            <div
+              className="menu-trigger"
+              onClick={() => {
+                setOpen(!open);
+              }}>
+              <img src={user} alt="User Icon" />
+            </div>
+
+            {/* Dropdown menu */}
+            <div className={`dropdown-menu ${open ? "active" : "inactive"}`}>
+              <ul>
+                {/* Rendering DropdownItem components with different destinations */}
+                <DropdownItem img={change_password} text={"Change Password"} destination="/change-password"/>
+                <DropdownItem img={logout} text={"Logout"} destination="/login" />
+              </ul>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
