57c847773ed21ebbc6ea2dbdf4ac490392c762cb
mustafa <zlac350@live.rhul.ac.uk>
2024-03-07 12:52:19 +0000

    Connected total number of assets feature card to backend

--- frontend/src/components/navigation/dashboard.jsx
+++ frontend/src/components/navigation/dashboard.jsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Navbar from "./Navbar"; // Make sure the path is correct
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Navbar from './Navbar';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
 
-import "../style.css";
-import "../Menustyle.css";
+import '../style.css';
+import '../Menustyle.css';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-function FeatureCard({ title, description }) {
+function FeatureCard({ title, description, subTitle }) {
   return (
     <div className="bg-white p-4 shadow-md rounded-lg m-2 transition-transform duration-300 hover:scale-105">
       <h2 className="text-lg font-bold mb-2">{title}</h2>
       <p>{description}</p>
+      {subTitle && <h3 className="text-xl font-bold mt-2">{subTitle}</h3>}
     </div>
   );
 }
@@ -23,36 +24,36 @@ function Dashboard({ username, userRole }) {
     labels: [],
     datasets: [
       {
-        label: "# of Asset Types",
+        label: '# of Asset Types',
         data: [],
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          // Add more colors as needed
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
         ],
         borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          // Add more border colors as needed
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
         ],
         borderWidth: 1,
       },
     ],
   });
 
+  const [totalAssets, setTotalAssets] = useState(0);
+
   useEffect(() => {
-    const fetchAssetTypes = async () => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:8080/assets/refresh"
-        );
+        const response = await axios.get('http://localhost:8080/assets/refresh');
         const assets = response.data;
+
+        setTotalAssets(assets.length);
 
         const typeCounts = assets.reduce((acc, asset) => {
           const { type_name } = asset.asset_type || {};
@@ -72,24 +73,23 @@ function Dashboard({ username, userRole }) {
           ],
         });
       } catch (error) {
-        console.error("Failed to fetch asset types:", error);
+        console.error('Failed to fetch data:', error);
       }
     };
 
-    fetchAssetTypes();
+    fetchData();
   }, []);
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Navbar userRole={userRole} username={username} />
       <main className="flex-grow p-8">
-        <h1 className="text-2xl font-semibold mb-4">
-          IT Asset Management Dashboard
-        </h1>
+        <h1 className="text-2xl font-semibold mb-4">IT Asset Management Dashboard</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           <FeatureCard
             title="Total Assets"
             description="Overview of all assets in the system."
+            subTitle={`Total: ${totalAssets}`}
           />
           <FeatureCard
             title="Issues Reported"
