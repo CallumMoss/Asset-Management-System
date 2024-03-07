32595f7d7961706bd890eb6ae85d74908410539d
Tom <zlac316@live.rhul.ac.uk>
2024-03-07 15:05:15 +0000

    Chat functionality

---
+++ project/src/main/java/cs2815/project/controller/ChatBoardController.java
@@ -0,0 +1,29 @@
+package cs2815.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cs2815.project.model.ChatBoard;
+import cs2815.project.service.ChatService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/messages")
+public class ChatBoardController {
+
+    @Autowired
+    private ChatService chatService;
+
+    @GetMapping("/refresh/{asset_id}")
+    public ResponseEntity<List<ChatBoard>> refreshMessages(@PathVariable int asset_id) {
+        List<ChatBoard> messages = chatService.refresh(asset_id);
+        return ResponseEntity.ok(messages);
+
+    }
+
+}
