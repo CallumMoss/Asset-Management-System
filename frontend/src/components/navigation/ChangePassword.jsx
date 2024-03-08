888b6b31b7b331727e6370adb26660ea23db4c74
zlac214 <zlac214@live.rhul.ac.uk>
2024-03-08 05:24:00 +0000

    Attempting to fix navbar in change password, removed themeprovider

--- frontend/src/components/navigation/ChangePassword.jsx
+++ frontend/src/components/navigation/ChangePassword.jsx
@@ -1,24 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import axios from "axios";
 import Navbar from "./Navbar";
-
-// Additional imports for MUI components
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import Chip from "@mui/material/Chip";
-import Checkbox from "@mui/material/Checkbox";
-import ListItemText from "@mui/material/ListItemText";
-
-// Create a default MUI theme
-const defaultTheme = createTheme();
 
 // Main component to change password
 function ChangePassword({ username, userRole }) {
@@ -86,8 +75,7 @@ function ChangePassword({ username, userRole }) {
 
   // Render the component
   return (
-    <ThemeProvider theme={defaultTheme}>
-      {/* Render Navbar component with userRole and username props */}
+    <>
       <Navbar userRole={userRole} username={username} />
       <Container component="main" maxWidth="sm">
         <CssBaseline />
@@ -97,7 +85,8 @@ function ChangePassword({ username, userRole }) {
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-          }}>
+          }}
+        >
           <Typography component="h1" variant="h5">
             Change Password
           </Typography>
@@ -106,7 +95,8 @@ function ChangePassword({ username, userRole }) {
             component="form"
             onSubmit={handleSubmit}
             noValidate
-            sx={{ mt: 1 }}>
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -149,13 +139,14 @@ function ChangePassword({ username, userRole }) {
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 }}>
+              sx={{ mt: 3, mb: 2 }}
+            >
               Submit
             </Button>
           </Box>
         </Box>
       </Container>
-    </ThemeProvider>
+    </>
   );
 }
 
--- frontend/src/components/navigation/Navbar.jsx
+++ frontend/src/components/navigation/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate, Link } from "react-router-dom";
-import ChangePassword from "./ChangePassword";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
