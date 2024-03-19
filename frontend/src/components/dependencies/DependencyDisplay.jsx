8fd5037f6a4a329d94a18751545a25cb3a05506f
Tom <zlac316@live.rhul.ac.uk>
2024-03-19 20:13:27 +0000

    group by display

--- frontend/src/components/dependencies/DependencyDisplay.jsx
+++ frontend/src/components/dependencies/DependencyDisplay.jsx
@@ -15,28 +15,45 @@ import {
 function DependencyDisplay({ dependencyList }) {
   const navigate = useNavigate();
 
+  // Group dependencies by parent asset
+  const groupedDependencies = dependencyList.reduce((acc, dependency) => {
+    const parentAsset = dependency.asset.title;
+    if (!acc[parentAsset]) {
+      acc[parentAsset] = [];
+    }
+    acc[parentAsset].push(dependency);
+    return acc;
+  }, {});
+
   return (
     <Container component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell style={{ fontWeight: "bold" }}>
-              Dependent Asset
-            </TableCell>
-            <TableCell style={{ fontWeight: "bold" }}>Realtionship</TableCell>
-            <TableCell style={{ fontWeight: "bold" }}>Parent Asset</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {dependencyList.map((dependency) => (
-            <TableRow key={dependency.id}>
-              <TableCell>{dependency.asset.title}</TableCell>
-              <TableCell>{dependency.relationType}</TableCell>
-              <TableCell>{dependency.dependent.title}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      {Object.keys(groupedDependencies).map((parentAsset) => (
+        <div key={parentAsset}>
+          <h2 style={{ fontWeight: "bold", marginTop: "20px" }}>
+            Parent Asset: {parentAsset}
+          </h2>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ fontWeight: "bold" }}>
+                  Relationship
+                </TableCell>
+                <TableCell style={{ fontWeight: "bold" }}>
+                  Dependent Asset
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {groupedDependencies[parentAsset].map((dependency) => (
+                <TableRow key={dependency.id}>
+                  <TableCell>{dependency.relationType}</TableCell>
+                  <TableCell>{dependency.dependent.title}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      ))}
     </Container>
   );
 }
