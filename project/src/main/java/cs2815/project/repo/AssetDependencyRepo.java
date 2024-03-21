d3223acc46bdb1dad9c1cfba5831e1481781d3d7
zlac214 <zlac214@live.rhul.ac.uk>
2024-03-21 07:02:47 +0000

    Adding javadoc to repo files.

--- project/src/main/java/cs2815/project/repo/AssetDependencyRepo.java
+++ project/src/main/java/cs2815/project/repo/AssetDependencyRepo.java
@@ -1,9 +1,18 @@
 package cs2815.project.repo;
 
+/*
+ * Import for project:
+ */
 import cs2815.project.model.AssetDependency;
 
+/*
+ * Java import:
+ */
 import java.util.List;
 
+/*
+ * Springboot imports:
+ */
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,14 +20,23 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Interface for AssetDependencies:
+ */
 @RepositoryRestResource
 public interface AssetDependencyRepo extends JpaRepository<AssetDependency, Integer> {
 
+    /*
+     * SQL query for deleting AssetDependency.
+     */
     @Modifying
     @Transactional
     @Query("DELETE FROM AssetDependency ad WHERE ad.asset.asset_id = :assetID OR ad.dependent.asset_id = :assetID")
     void deleteAssetbyID(@Param("assetID") int assetID);
 
+    /*
+     * SQL query for selecting AssetDependency.
+     */
     @Query("SELECT a FROM AssetDependency a WHERE a.dependent.asset_id = :assetId")
     List<AssetDependency> findParentAsset(@Param("assetId") int assetId);
 }
--- project/src/main/java/cs2815/project/repo/LangRepo.java
+++ project/src/main/java/cs2815/project/repo/LangRepo.java
@@ -1,7 +1,13 @@
 package cs2815.project.repo;
 
+/*
+ * Imports for project.
+ */
 import cs2815.project.model.Languages;
 
+/*
+ * Springboot imports:
+ */
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,28 +15,58 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
+/*
+ * Java imports:
+ */
 import java.util.List;
 
+/**
+ * Interface for Language repository.
+ */
 @RepositoryRestResource
 public interface LangRepo extends JpaRepository<Languages, Integer> { // Integer being the type of the primary key
-
+   
+    /**
+     * Retrieves all language names.
+     *
+     * @return A list of all language names.
+     */
     @Query("SELECT l.language_name FROM Languages l ")
     List<String> getAllLanguageNames();
 
+    /**
+     * Finds a language by its ID.
+     *
+     * @param langID The ID of the language.
+     * @return The language with the specified ID.
+     */
     @Query("SELECT l FROM Languages l WHERE l.language_id = :langID")
     Languages findLanguageById(@Param("langID") int langID);
 
+    /**
+     * Finds a language by its name.
+     *
+     * @param langName The name of the language.
+     * @return The language with the specified name.
+     */
     @Query("SELECT l FROM Languages l WHERE l.language_name = :langName")
     Languages findLanguageByName(@Param("langName") String langName);
 
+    /**
+     * Deletes a language by its ID.
+     *
+     * @param langID The ID of the language to be deleted.
+     */
     @Modifying
     @Transactional
     @Query("DELETE FROM Languages l WHERE l.language_id = :langID")
     void deleteLanguageByID(@Param("langID") int langID);
 
+    /**
+     * Retrieves all languages.
+     *
+     * @return A list of all languages.
+     */
     @Query("SELECT l FROM Languages l")
     List<Languages> getLanguages();
-
-
-
 }
