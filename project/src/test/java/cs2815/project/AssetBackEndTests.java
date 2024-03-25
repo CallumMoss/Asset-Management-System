e6e1c4e4a9a65ed9ef6dfef6d4101c74374b4f95
zlac354 <zlac354@live.rhul.ac.uk>
2024-03-25 22:46:08 +0000

    Added a test for filter search by asset type.

--- project/src/test/java/cs2815/project/AssetBackEndTests.java
+++ project/src/test/java/cs2815/project/AssetBackEndTests.java
@@ -79,7 +79,7 @@ class AssetBackEndTests {
         }
     }
 
-        @Test
+    @Test
     void test2() {
         // Testing default search (by name)
         List<Asset> searched_assets;
@@ -128,6 +128,62 @@ class AssetBackEndTests {
         expected_assets.add(asset1);
         expected_assets.add(asset2);
         expected_assets.add(asset3);
+
+        for (int i = 0; i < 3; i++) {
+            assert(searched_assets.get(i).equals(expected_assets.get(i)));
+        }
+        expected_assets.clear();
+    }
+
+    @Test
+    void test3() {
+        // Testing default search (by type)
+        List<Asset> searched_assets;
+
+        List<Asset> expected_assets = new ArrayList<Asset>();
+        
+        List<String> authors = Arrays.asList("BaseAdmin");
+        DependencyWrapper dwrapper = new DependencyWrapper();
+        List<DependencyWrapper> dwrapper_list = new ArrayList<DependencyWrapper>();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper1 = new AssetWrapper(1, "Piece.py",
+                "A python program that contains a class which describes the attributes and functions of a chess piece.",
+                "website.com/piece.py", "Python File", authors, dwrapper_list, "3.9.10", null, null);
+        Asset asset1 = ai.convertWrapperToAsset(wrapper1);
+
+        authors = Arrays.asList("BaseViewer");
+        dwrapper = new DependencyWrapper();
+        dwrapper_list.clear();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper2 = new AssetWrapper(2, "Heroes Rising", "2D Game developed as part of the first year games module.",
+                "some_link.com", "Project", authors, dwrapper_list, "Callum and Satwik", "DongGyun", "Complete");
+        Asset asset2 = ai.convertWrapperToAsset(wrapper2);
+
+        authors = Arrays.asList("BaseUser", "BaseViewer");
+        dwrapper = new DependencyWrapper("Heroes Rising", "Documentation of");
+        dwrapper_list.clear();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper3 = new AssetWrapper(3, "README", "Read me file for the project Heroes Rising.", "random/readme.md",
+                "Documentation", authors, dwrapper_list, ".MD", "Outlines details relevant for product use", null);
+        Asset asset3 = ai.convertWrapperToAsset(wrapper3);
+
+
+
+        // Testing exact match for one
+        searched_assets = ai.searchByType("Python File");
+        expected_assets.add(asset1);
+        assert(searched_assets.get(0).equals(expected_assets.get(0)));
+
+        // Testing similar match for one
+        searched_assets = ai.searchByType("ytho");
+        assert(searched_assets.get(0).equals(expected_assets.get(0)));
+        expected_assets.clear();
+
+        // Testing similar match for all
+        searched_assets = ai.searchByType("e");
+        expected_assets.add(asset1);
+        expected_assets.add(asset3); // different order because documentation is created before project asset type, so is displayed in this order
+        expected_assets.add(asset2);
 
         for (int i = 0; i < 3; i++) {
             assert(searched_assets.get(i).equals(expected_assets.get(i)));
