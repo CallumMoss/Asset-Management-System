c30b3757926603d373c6fef17e1490ef61be7a46
zlac354 <zlac354@live.rhul.ac.uk>
2024-03-26 19:03:39 +0000

    Added testing for oldest and newest asset sort.

--- project/src/test/java/cs2815/project/AssetBackEndTests.java
+++ project/src/test/java/cs2815/project/AssetBackEndTests.java
@@ -11,12 +11,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import cs2815.project.controller.AssetController;
 import cs2815.project.model.Asset;
-import cs2815.project.model.AssetType;
 import cs2815.project.model.specialmodels.AssetWrapper;
 import cs2815.project.model.specialmodels.DependencyWrapper;
 import cs2815.project.repo.AssetRepo;
@@ -294,6 +291,92 @@ class AssetBackEndTests {
         expected_assets.add(asset2);
         expected_assets.add(asset1);
         expected_assets.add(asset3);
+
+        for (int i = 0; i < 3; i++) {
+            assert(sorted_assets.get(i).equals(expected_assets.get(i)));
+        }
+    }
+
+    @Test
+    void test6() {
+        // Testing chronological sort (oldest first)
+        List<Asset> sorted_assets;
+
+        List<Asset> expected_assets = new ArrayList<Asset>();
+        
+        List<String> authors = Arrays.asList("BaseAdmin");
+        DependencyWrapper dwrapper = new DependencyWrapper();
+        List<DependencyWrapper> dwrapper_list = new ArrayList<DependencyWrapper>();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper1 = new AssetWrapper(1, "Piece.py",
+                "A python program that contains a class which describes the attributes and functions of a chess piece.",
+                "website.com/piece.py", "Python File", authors, dwrapper_list, "3.9.10", null, null);
+        Asset asset1 = ai.convertWrapperToAsset(wrapper1);
+
+        authors = Arrays.asList("BaseViewer");
+        dwrapper = new DependencyWrapper();
+        dwrapper_list.clear();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper2 = new AssetWrapper(2, "Heroes Rising", "2D Game developed as part of the first year games module.",
+                "some_link.com", "Project", authors, dwrapper_list, "Callum and Satwik", "DongGyun", "Complete");
+        Asset asset2 = ai.convertWrapperToAsset(wrapper2);
+
+        authors = Arrays.asList("BaseUser", "BaseViewer");
+        dwrapper = new DependencyWrapper("Heroes Rising", "Documentation of");
+        dwrapper_list.clear();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper3 = new AssetWrapper(3, "README", "Read me file for the project Heroes Rising.", "random/readme.md",
+                "Documentation", authors, dwrapper_list, ".MD", "Outlines details relevant for product use", null);
+        Asset asset3 = ai.convertWrapperToAsset(wrapper3);
+
+        sorted_assets = ai.sort(ai.refresh(), "Oldest");
+        
+        expected_assets.add(asset1);
+        expected_assets.add(asset2);
+        expected_assets.add(asset3);
+
+        for (int i = 0; i < 3; i++) {
+            assert(sorted_assets.get(i).equals(expected_assets.get(i)));
+        }
+    }
+
+    @Test
+    void test7() {
+        // Testing chronological sort (newest first)
+        List<Asset> sorted_assets;
+
+        List<Asset> expected_assets = new ArrayList<Asset>();
+        
+        List<String> authors = Arrays.asList("BaseAdmin");
+        DependencyWrapper dwrapper = new DependencyWrapper();
+        List<DependencyWrapper> dwrapper_list = new ArrayList<DependencyWrapper>();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper1 = new AssetWrapper(1, "Piece.py",
+                "A python program that contains a class which describes the attributes and functions of a chess piece.",
+                "website.com/piece.py", "Python File", authors, dwrapper_list, "3.9.10", null, null);
+        Asset asset1 = ai.convertWrapperToAsset(wrapper1);
+
+        authors = Arrays.asList("BaseViewer");
+        dwrapper = new DependencyWrapper();
+        dwrapper_list.clear();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper2 = new AssetWrapper(2, "Heroes Rising", "2D Game developed as part of the first year games module.",
+                "some_link.com", "Project", authors, dwrapper_list, "Callum and Satwik", "DongGyun", "Complete");
+        Asset asset2 = ai.convertWrapperToAsset(wrapper2);
+
+        authors = Arrays.asList("BaseUser", "BaseViewer");
+        dwrapper = new DependencyWrapper("Heroes Rising", "Documentation of");
+        dwrapper_list.clear();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper3 = new AssetWrapper(3, "README", "Read me file for the project Heroes Rising.", "random/readme.md",
+                "Documentation", authors, dwrapper_list, ".MD", "Outlines details relevant for product use", null);
+        Asset asset3 = ai.convertWrapperToAsset(wrapper3);
+
+        sorted_assets = ai.sort(ai.refresh(), "Newest");
+        
+        expected_assets.add(asset3);
+        expected_assets.add(asset2);
+        expected_assets.add(asset1);
 
         for (int i = 0; i < 3; i++) {
             assert(sorted_assets.get(i).equals(expected_assets.get(i)));
