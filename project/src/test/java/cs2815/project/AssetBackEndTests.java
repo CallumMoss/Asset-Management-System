d434f3016adb8057b37852857f871a079e2f0308
zlac354 <zlac354@live.rhul.ac.uk>
2024-03-26 18:45:38 +0000

    Added type filter test

--- project/src/test/java/cs2815/project/AssetBackEndTests.java
+++ project/src/test/java/cs2815/project/AssetBackEndTests.java
@@ -191,5 +191,85 @@ class AssetBackEndTests {
         expected_assets.clear();
     }
 
+    @Test
+    void test4() {
+        // Testing default search (by author)
+        List<Asset> searched_assets;
+
+        List<Asset> expected_assets = new ArrayList<Asset>();
+        
+        List<String> authors = Arrays.asList("BaseAdmin");
+        DependencyWrapper dwrapper = new DependencyWrapper();
+        List<DependencyWrapper> dwrapper_list = new ArrayList<DependencyWrapper>();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper1 = new AssetWrapper(1, "Piece.py",
+                "A python program that contains a class which describes the attributes and functions of a chess piece.",
+                "website.com/piece.py", "Python File", authors, dwrapper_list, "3.9.10", null, null);
+        Asset asset1 = ai.convertWrapperToAsset(wrapper1);
+
+        authors = Arrays.asList("BaseViewer");
+        dwrapper = new DependencyWrapper();
+        dwrapper_list.clear();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper2 = new AssetWrapper(2, "Heroes Rising", "2D Game developed as part of the first year games module.",
+                "some_link.com", "Project", authors, dwrapper_list, "Callum and Satwik", "DongGyun", "Complete");
+        Asset asset2 = ai.convertWrapperToAsset(wrapper2);
+
+        authors = Arrays.asList("BaseUser", "BaseViewer");
+        dwrapper = new DependencyWrapper("Heroes Rising", "Documentation of");
+        dwrapper_list.clear();
+        dwrapper_list.add(dwrapper);
+        AssetWrapper wrapper3 = new AssetWrapper(3, "README", "Read me file for the project Heroes Rising.", "random/readme.md",
+                "Documentation", authors, dwrapper_list, ".MD", "Outlines details relevant for product use", null);
+        Asset asset3 = ai.convertWrapperToAsset(wrapper3);
+
+
+
+        // Testing exact match for one
+        searched_assets = ai.searchByAuthor("BaseAdmin");
+        expected_assets.add(asset1);
+        assert(searched_assets.get(0).equals(expected_assets.get(0)));
+
+        // Testing similar match for one
+        searched_assets = ai.searchByAuthor("admi");
+        assert(searched_assets.get(0).equals(expected_assets.get(0)));
+        expected_assets.clear();
+
+        // Testing similar match for all
+        searched_assets = ai.searchByAuthor("Base");
+        expected_assets.add(asset2);
+        expected_assets.add(asset3); // different order because documentation is created before project asset type, so is displayed in this order
+        expected_assets.add(asset1);
+
+        for (int i = 0; i < 3; i++) {
+                //System.out.println(searched_assets.get(i).getTitle());
+                //System.out.println(expected_assets.get(i).getTitle());
+            assert(searched_assets.get(i).equals(expected_assets.get(i)));
+        }
+        expected_assets.clear();
+
+        // Testing for multiple authors
+        // BaseViewer and BaseUser
+        List<Asset> searched_assets1 = ai.searchByAuthor("BaseViewer");
+        List<Asset> searched_assets2 = ai.searchByAuthor("BaseUser");
+
+        expected_assets.add(asset2);
+        expected_assets.add(asset3);
+        
+        System.out.println(searched_assets1.get(0).getTitle());
+        System.out.println(expected_assets.get(0).getTitle());
+        System.out.println(searched_assets.get(0).getTitle());
+        System.out.println(expected_assets.get(1).getTitle());
+        System.out.println(searched_assets.get(1).getTitle());
+        System.out.println(expected_assets.get(1).getTitle());
+
+        assert(searched_assets1.get(0).equals(expected_assets.get(0)));
+        assert(searched_assets2.get(0).equals(expected_assets.get(1)));
+        assert(searched_assets1.get(1).equals(expected_assets.get(1)));
+
+        // searched_assets 1 and 2
+    }
+
+
     // Testing incorrect cases (such as creating an asset that already exists)
 }
